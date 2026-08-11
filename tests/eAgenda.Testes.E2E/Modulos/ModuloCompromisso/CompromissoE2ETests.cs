e0800cc9a1db805169182b6f4a40def10ec4cc30
Natalia Bortoli Vieira <nataliabortolivieira@gmail.com>
2026-08-11 19:00:31 -0300

    Adiciona estrutura inicial para testes E2E

---
+++ tests/eAgenda.Testes.E2E/Modulos/ModuloCompromisso/CompromissoE2ETests.cs
@@ -0,0 +1,8 @@
+using System;
+
+namespace eAgenda.Testes.E2E.Modulos.ModuloCompromisso;
+
+public class CompromissoE2ETests
+{
+
+}
---
+++ tests/eAgenda.Testes.E2E/Modulos/ModuloContato/ContatoE2ETests.cs
@@ -0,0 +1,8 @@
+using System;
+
+namespace eAgenda.Testes.E2E.Modulos.ModuloContato;
+
+public class ContatoE2ETests
+{
+
+}
